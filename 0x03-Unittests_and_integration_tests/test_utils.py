df34cc277643147e7848a27ffb15c151502016f8
SaharMFR <sweetysahora@gmail.com>
2024-07-01 22:53:00 +0000

    Task-3 0x03

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Unittests and Integration Tests """
 import unittest
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 from typing import Mapping, Sequence, Union, Dict
 from unittest.mock import patch
@@ -43,3 +43,22 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get") as request:
             request.return_value.json.return_value = test_payload
             self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Tests `utils.memoize` function """
+    def test_memoize(self) -> None:
+        """ Tests `memoize` """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, "a_method") as memo:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            memo.assert_called_once()
